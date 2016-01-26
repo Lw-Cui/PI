043f39723e3525abd93227716ee05220f5c28b2b
Lw-Cui <6457739460@QQ.com>
2016-01-26 20:23:35 +0800

    implement addition, substraction, multiply and divide

---
+++ Pi.cpp
@@ -0,0 +1,93 @@
+#include <cstdio>
+#include <cassert>
+const int shift = 28;
+const unsigned BASE = 1 << shift;
+const int LEN = 10000000;
+
+void add(unsigned dest[], unsigned src[]) {
+	for (int i = LEN - 1; i >= 0; i--)
+		if (dest[i] >= BASE - src[i]) {
+			assert(i && "Oh carry overflow!");
+			dest[i] = (long long)dest[i] + src[i] - BASE;
+			dest[i - 1]++;
+		} else {
+			dest[i] += src[i];
+		}
+}
+
+void minus(unsigned dest[], unsigned src[]) {
+	bool borrow = false;
+	for (int i = LEN - 1; i >= 0; i--)
+		if (dest[i] < src[i] + (borrow? 1: 0)) {
+			assert(i && "Oh borrow overflow!");
+			dest[i] = (long long)dest[i] + BASE - src[i] - (borrow? 1: 0);
+			borrow = true;
+		} else {
+			dest[i] = dest[i] - src[i] - (borrow? 1: 0);
+			borrow = false;
+		}
+}
+
+void multiply(unsigned dest[], unsigned num) {
+	unsigned carry = 0;
+	for (int i = LEN - 1; i >= 0; i--) {
+		unsigned long long tmp = (long long)num * dest[i];
+		dest[i] = (tmp & (BASE - 1)) + carry;
+		carry = tmp >> shift;
+		assert((i || !carry) && "Oh multiply overflow!");
+	}
+}
+
+void divide(unsigned dest[], unsigned num) {
+	unsigned long long s = 0;
+	for (int i = 0; i < LEN; i++) {
+		s <<= shift;
+		s += dest[i];
+		if (s >= num) {
+			dest[i] = s / num;
+			s %= num;
+		} else {
+			dest[i] = 0;
+		}
+	}
+}
+
+void test_multiply_divide() {
+	unsigned *ptr = new unsigned[LEN];
+	for (int i = 0; i < LEN; i++)
+		ptr[i] = i;
+	unsigned num = 123454321;
+	multiply(ptr, num);
+	divide(ptr, num);
+	for (size_t i = 0; i < LEN; i++)
+		assert(ptr[i] == i && "multiply or divide error.");
+	delete[] ptr;
+}
+
+void test_add_minus() {
+	unsigned *ptr = new unsigned[LEN], *ptr2 = new unsigned[LEN];
+	for (size_t i = 0; i < LEN; i++) {
+		ptr[i] = LEN - i;
+		ptr2[i] = ptr[i] + 1;
+	}
+	ptr[0] = 1, ptr2[0] = 0;
+
+	minus(ptr, ptr2);
+	assert(ptr[0] == 0 && "ptr[0] minus error.");
+	assert(ptr[LEN - 1] == BASE - 1 && "ptr[LEN - 1] minus error.");
+	for (int i = 1; i < LEN - 1; i++)
+		assert(ptr[i] == BASE - 2 && "substraction error.");
+
+	add(ptr, ptr2);
+	assert(ptr[0] == 1 && "ptr[0] add error.");
+	for (size_t i = 1; i < LEN; i++)
+		assert(ptr[i] == LEN - i && "addition error.");
+	delete[] ptr;
+	delete[] ptr2;
+}
+
+int main(int argc, char *argv[]) {
+	test_add_minus();
+	test_multiply_divide();
+	return 0;
+}
