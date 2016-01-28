d37a60d39387082fb63e55b990e55917beabf74a
Lw-Cui <6457739460@QQ.com>
2016-01-28 22:54:55 +0800

    unit test optimization

--- Pi.cpp
+++ Pi.cpp
@@ -58,41 +58,6 @@ void divide(unsigned dest[], unsigned num) {
 	}
 }
 
-void test_multiply_divide() {
-	unsigned *ptr = new unsigned[LEN];
-	for (int i = 0; i < LEN; i++)
-		ptr[i] = i;
-	unsigned num = 123454321;
-	multiply(ptr, num);
-	divide(ptr, num);
-	for (int i = 0; i < LEN; i++)
-		assert(ptr[i] == (unsigned)i && "multiply or divide error.");
-	delete[] ptr;
-}
-
-void test_add_minus() {
-	unsigned long long *ptr = new unsigned long long[LEN];
-	unsigned *ptr2 = new unsigned[LEN];
-	for (int i = 0; i < LEN; i++) {
-		ptr[i] = LEN - i;
-		ptr2[i] = ptr[i] + 1;
-	}
-	ptr[0] = 1, ptr2[0] = 0;
-
-	minus(ptr, ptr2);
-	assert(ptr[0] == 0 && "ptr[0] minus error.");
-	assert(ptr[LEN - 1] == BASE - 1 && "ptr[LEN - 1] minus error.");
-	for (int i = 1; i < LEN - 1; i++)
-		assert(ptr[i] == BASE - 2 && "substraction error.");
-
-	add(ptr, ptr2);
-	assert(ptr[0] == 1 && "ptr[0] add error.");
-	for (int i = 1; i < LEN; i++)
-		assert(ptr[i] == (unsigned)LEN - i && "addition error.");
-	delete[] ptr;
-	delete[] ptr2;
-}
-
 
 inline void print(unsigned num) {
 	for (int pos = SHIFT - 4; pos >= 0; pos -= 4)
@@ -134,35 +99,6 @@ void shift_right(unsigned sub[], unsigned num) {
 		sub[i] |= (sub[i - 1] & mask) << (uint_bit - mod);
 	}
 	sub[0] >>= mod;
-}
-
-//TODO
-void test_shift_right() {
-	unsigned *sub = new unsigned[LEN];
-	for (int i = 0; i < LEN; i++)
-		sub[i] = 0xFFFFFFFF;
-	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 1) + 6);
-	assert(sub[LEN - 1] == 0x3FFFFFF && "shift error.");
-	for (int i = 0; i < LEN - 1; i++)
-		assert(sub[i] == 0 && "shift error.");
-
-	for (int i = 0; i < LEN; i++)
-		sub[i] = 0xABCDDCBE;
-	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 2) + 4);
-	assert(sub[LEN - 2] == 0xABCDDCB && "shift error.");
-	assert(sub[LEN - 1] == 0xEABCDDCB  && "shift error.");
-	for (int i = 0; i < LEN - 2; i++)
-		assert(sub[i] == 0 && "shift error.");
-	delete[] sub;
-
-	sub = new unsigned[3];
-	sub[0] = 0;
-	sub[1] = 0x19999999;
-	sub[2] = 0x99999999;
-	shift_right(sub, 8);
-	assert(sub[1] == 0x00199999 && "shift error!");
-	assert(sub[2] == 0x99999999 && "shift error!");
-	delete[] sub;
 }
 
 void cal_fraction(unsigned sub[], unsigned numerator, unsigned denominator,
@@ -219,6 +155,15 @@ bool cal_sub_Pi(unsigned long long Pi[], unsigned k) {
 
 }
 
+void Pi_carry(unsigned long long Pi[]) {
+	for (int i = LEN - 1; i >= 0; i--)
+		if (Pi[i] >= BASE) {
+			assert(i && "long long Pi overflow.");
+			Pi[i - 1] += (Pi[i] & ~(BASE - 1)) >> SHIFT;
+			Pi[i] &= (BASE - 1);
+		}
+}
+
 /*
    BBP formula
 bool cal_sub_Pi(unsigned long long Pi[], unsigned k) {
@@ -263,15 +208,8 @@ bool cal_sub_Pi(unsigned long long Pi[], unsigned k) {
 }
 */
 
-void Pi_carry(unsigned long long Pi[]) {
-	for (int i = LEN - 1; i >= 0; i--)
-		if (Pi[i] >= BASE) {
-			assert(i && "long long Pi overflow.");
-			Pi[i - 1] += (Pi[i] & ~(BASE - 1)) >> SHIFT;
-			Pi[i] &= (BASE - 1);
-		}
-}
 
+#ifdef DEBUG
 void test_carray() {
 	unsigned long long *ptr = new unsigned long long[LEN];
 	ptr[0] = 0;
@@ -282,14 +220,76 @@ void test_carray() {
 	delete[] ptr;
 }
 
+void test_multiply_divide() {
+	unsigned *ptr = new unsigned[LEN];
+	for (int i = 0; i < LEN; i++)
+		ptr[i] = i;
+	unsigned num = 123454321;
+	multiply(ptr, num);
+	divide(ptr, num);
+	for (int i = 0; i < LEN; i++)
+		assert(ptr[i] == (unsigned)i && "multiply or divide error.");
+	delete[] ptr;
+}
+
+void test_add_minus() {
+	unsigned long long *ptr = new unsigned long long[LEN];
+	unsigned *ptr2 = new unsigned[LEN];
+	for (int i = 0; i < LEN; i++) {
+		ptr[i] = LEN - i;
+		ptr2[i] = ptr[i] + 1;
+	}
+	ptr[0] = 1, ptr2[0] = 0;
+
+	minus(ptr, ptr2);
+	assert(ptr[0] == 0 && "ptr[0] minus error.");
+	assert(ptr[LEN - 1] == BASE - 1 && "ptr[LEN - 1] minus error.");
+	for (int i = 1; i < LEN - 1; i++)
+		assert(ptr[i] == BASE - 2 && "substraction error.");
+
+	add(ptr, ptr2);
+	assert(ptr[0] == 1 && "ptr[0] add error.");
+	for (int i = 1; i < LEN; i++)
+		assert(ptr[i] == (unsigned)LEN - i && "addition error.");
+	delete[] ptr;
+	delete[] ptr2;
+}
+
+void test_shift_right() {
+	unsigned *sub = new unsigned[LEN];
+	for (int i = 0; i < LEN; i++)
+		sub[i] = 0xFFFFFFFF;
+	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 1) + 6);
+	assert(sub[LEN - 1] == 0x3FFFFFF && "shift error.");
+	for (int i = 0; i < LEN - 1; i++)
+		assert(sub[i] == 0 && "shift error.");
+
+	for (int i = 0; i < LEN; i++)
+		sub[i] = 0xABCDDCBE;
+	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 2) + 4);
+	assert(sub[LEN - 2] == 0xABCDDCB && "shift error.");
+	assert(sub[LEN - 1] == 0xEABCDDCB  && "shift error.");
+	for (int i = 0; i < LEN - 2; i++)
+		assert(sub[i] == 0 && "shift error.");
+
+	sub[0] = 0;
+	sub[1] = 0x19999999;
+	sub[2] = 0x99999999;
+	shift_right(sub, 8);
+	assert(sub[1] == 0x00199999 && "shift error!");
+	assert(sub[2] == 0x99999999 && "shift error!");
+	delete[] sub;
+}
+#endif
+
 int main(int argc, char *argv[]) {
-	/*
+#ifdef DEBUG
 	test_add_minus();
 	test_multiply_divide();
 	test_print();
 	test_shift_right();
 	test_carray();
-	*/
+#else
 	double elapsed_time;
 	MPI_Init(&argc, &argv);
 	if (argc == 2) {
@@ -319,5 +319,6 @@ int main(int argc, char *argv[]) {
 	delete[] sub_Pi;
 	delete[] Pi;
 	MPI_Finalize();
+#endif
 	return 0;
 }
