8eaed26f2340a4dd6af5e32e480b655de88303a5
Lw-Cui <645739460@QQ.com>
2016-02-08 11:13:47 +0800

    google test

--- Makefile
+++ Makefile
@@ -1,17 +1,27 @@
 SRC :=	${wildcard *.cpp}
 HEAD:=	${wildcard *.h}
-OBJ	:=	${patsubst %.cpp, %.o, $(SRC)}
 OP	:=	Operation.o
 MPI	:=	Pi_mpi.o
 OMP	:=	Pi_omp.o
 TEST:=	Pi_test.o
-
 CC	:=	mpic++
-CFLAG := -g -O2 -Wall -fopenmp
+CFLAG := -g -Wall -Wextra -pthread -fopenmp -I.
 
 all: test mpi omp
 
-$(MPI) $(OMP) $(TEST) $(OP): %.o: %.cpp $(HEAD)
+GTEST_HEADERS = gtest/*.h gtest/internal/*.h
+GTEST_SRCS_ = src/*.cc src/*.h $(GTEST_HEADERS)
+
+gtest-all.o : $(GTEST_SRCS_)
+	$(CC) $(CFLAG) -c src/gtest-all.cc
+
+gtest_main.o : $(GTEST_SRCS_)
+	$(CC) $(CFLAG) -c src/gtest_main.cc
+
+gtest_main.a : gtest-all.o gtest_main.o
+	$(AR) $(ARFLAGS) $@ $^
+
+$(MPI) $(OMP) $(TEST) $(OP): %.o: %.cpp $(HEAD) $(GTEST_HEADERS)
 	$(CC) $< -c -o $@ $(CFLAG)
 
 omp: $(OP) $(OMP)
@@ -20,11 +30,11 @@ omp: $(OP) $(OMP)
 mpi: $(OP) $(MPI)
 	$(CC) $^ -o $@.out $(CFLAG)
 
-test: $(OP) $(TEST)
-	$(CC) $^ -o $@.out $(CFLAG)
+test:	$(TEST) $(OP) gtest_main.a
+	$(CC) $(CFLAG) -lpthread $^ -o $@.out
 
 clean:
-	rm $(OBJ)
+	rm -rf *.a *.o *.out
 
 rebuild:	clean all
 
--- Pi_test.cpp
+++ Pi_test.cpp
@@ -1,23 +1,22 @@
 #include <cstdio>
 #include <cstdlib>
-#include <mpi.h>
 #include <cstring>
-#include <cassert>
 #include "Operation.h"
+#include "gtest/gtest.h"
 int VALID = 1000000;
 int LEN = VALID / 8 + 2;
 
-void test_carray() {
+TEST(CarrayTest, add) {
 	unsigned long long *ptr = new unsigned long long[LEN];
 	ptr[0] = 0;
 	ptr[1] = 0xFFFFFFFFFF;
 	Pi_carry(ptr);
-	assert(ptr[0] == 0xFF && "carry error.");
-	assert(ptr[1] == 0xFFFFFFFF && "carry error.");
+	EXPECT_EQ(0xFF, ptr[0]);
+	EXPECT_EQ(0xFFFFFFFF, ptr[1]);
 	delete[] ptr;
 }
 
-void test_multiply_divide() {
+TEST(divideTest, multi) {
 	unsigned *ptr = new unsigned[LEN];
 	for (int i = 0; i < LEN; i++)
 		ptr[i] = i;
@@ -25,11 +24,11 @@ void test_multiply_divide() {
 	multiply(ptr, num);
 	divide(ptr, num);
 	for (int i = 0; i < LEN; i++)
-		assert(ptr[i] == (unsigned)i && "multiply or divide error.");
+		EXPECT_EQ(i, ptr[i]);
 	delete[] ptr;
 }
 
-void test_add_minus() {
+TEST(AddTest, minus) {
 	unsigned long long *ptr = new unsigned long long[LEN];
 	unsigned *ptr2 = new unsigned[LEN];
 	for (int i = 0; i < LEN; i++) {
@@ -39,55 +38,48 @@ void test_add_minus() {
 	ptr[0] = 1, ptr2[0] = 0;
 
 	minus(ptr, ptr2);
-	assert(ptr[0] == 0 && "ptr[0] minus error.");
-	assert(ptr[LEN - 1] == BASE - 1 && "ptr[LEN - 1] minus error.");
+	EXPECT_EQ(0, ptr[0]);
+	EXPECT_EQ(BASE - 1, ptr[LEN - 1]);
 	for (int i = 1; i < LEN - 1; i++)
-		assert(ptr[i] == BASE - 2 && "substraction error.");
+		EXPECT_EQ(BASE - 2, ptr[i]);
 
 	add(ptr, ptr2);
-	assert(ptr[0] == 1 && "ptr[0] add error.");
+	EXPECT_EQ(1, ptr[0]);
 	for (int i = 1; i < LEN; i++)
-		assert(ptr[i] == (unsigned)LEN - i && "addition error.");
+		EXPECT_EQ(LEN - i, ptr[i]);
 	delete[] ptr;
 	delete[] ptr2;
 }
 
-void test_shift_right() {
+TEST(ShiftTest, shift) {
 	unsigned *sub = new unsigned[LEN];
 	for (int i = 0; i < LEN; i++)
 		sub[i] = 0xFFFFFFFF;
 	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 1) + 6);
-	assert(sub[LEN - 1] == 0x3FFFFFF && "shift error.");
+	EXPECT_EQ(0x3FFFFFF, sub[LEN - 1]);
 	for (int i = 0; i < LEN - 1; i++)
-		assert(sub[i] == 0 && "shift error.");
+		EXPECT_EQ(0, sub[i]);
 
 	for (int i = 0; i < LEN; i++)
 		sub[i] = 0xABCDDCBE;
 	shift_right(sub, sizeof(unsigned) * 8 * (LEN - 2) + 4);
-	assert(sub[LEN - 2] == 0xABCDDCB && "shift error.");
-	assert(sub[LEN - 1] == 0xEABCDDCB  && "shift error.");
+	EXPECT_EQ(0xABCDDCB, sub[LEN - 2]);
+	EXPECT_EQ(0xEABCDDCB, sub[LEN - 1]);
 	for (int i = 0; i < LEN - 2; i++)
-		assert(sub[i] == 0 && "shift error.");
+		EXPECT_EQ(0, sub[i]);
 
 	sub[0] = 0;
 	sub[1] = 0x19999999;
 	sub[2] = 0x99999999;
 	shift_right(sub, 8);
-	assert(sub[1] == 0x00199999 && "shift error!");
-	assert(sub[2] == 0x99999999 && "shift error!");
+	EXPECT_EQ(0x00199999, sub[1]);
+	EXPECT_EQ(0x99999999, sub[2]);
 	delete[] sub;
 }
 
+/*
 void test_print() {
 	unsigned tmp = 180150013;
 	print(tmp);
 }
-
-int main(int argc, char *argv[]) {
-	test_add_minus();
-	test_multiply_divide();
-	test_print();
-	test_shift_right();
-	test_carray();
-	return 0;
-}
+*/
